9e7a65c88abebd9d5326aaf992b16847c3d7ec28
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2016-03-24 16:03:40 +0100

    ContactList: store segment id

--- src/Models/ContactList.php
+++ src/Models/ContactList.php
@@ -4,14 +4,23 @@ namespace Brief\Models;
 
 class ContactList extends \Brief\Model {
 
+	/** @var int */
 	public $id;
+
+	/** @var string */
 	public $name;
 
-	public function __construct($api, $data) {
+	/** @var int|NULL */
+	public $segment_id;
+
+
+	public function __construct($api, $data)
+	{
 		parent::__construct($api);
 
-		$this->id    = (int)    $data->id;
-		$this->name  = (string) $data->name;
+		$this->id = (int)    $data->id;
+		$this->name = (string) $data->name;
+		$this->segment_id = $data->segment_id instanceof \SimpleXMLElement ? NULL : (int) $data->segment_id;
 	}
 
 	public function getContacts() {
